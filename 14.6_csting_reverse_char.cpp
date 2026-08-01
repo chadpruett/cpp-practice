e4a0074999040fed5b760cedc6fe419ddf07d06d
chadpruett <chadpruett22@gmail.com>
2026-08-01 14:31:19 -0700

    fixed reverse and added print function

--- 14.6_csting_reverse_char.cpp
+++ 14.6_csting_reverse_char.cpp
@@ -5,6 +5,7 @@ const int MAX = 30;
 
 void getText(char text[]);
 void myStrReverse(char text[]);
+void printReverse(const char text[]);
 
 int main()
 {
@@ -12,7 +13,7 @@ int main()
 
 	getText(text);
 	myStrReverse(text);
-	//printResult(text);
+	printReverse(text);
 
 	return 0;
 }
@@ -26,18 +27,36 @@ void getText(char text[])
 void myStrReverse(char text[])
 {
 	int length = 0;
-
-	std::cout << "\nWord backwards: ";
+	int index = 0;
+	char tempChar;
 
 	while (text[length] != '\0')
 	{
 		length++;
 	}
-	
-	for (int i = length - 1; i >= 0; i--)
+
+	index = length;
+
+	for (int i = 0; i < length / 2; i++)
 	{
-		std::cout << text[i];
+		tempChar = text[i];
+		text[i] = text[index - 1];
+		text[index - 1] = tempChar;
+		index--;
 	}
 
 	std::cout << endl;
 }
+
+void printReverse(const char text[])
+{
+	std::cout << "\nWord Backwards: ";
+	
+	for (int i = 0; text[i] != '\0'; i++)
+	{
+		std::cout << text[i];
+	}
+
+	std::cout << "\n\n";
+}
+
